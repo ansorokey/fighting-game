18d3562ccae5fd560e8c7e29a342eddfe6aec8e2
ansorokey <ansorokey@gmail.com>
2024-08-17 21:30:19 -0400

    comments

--- assets/src/fighter.js
+++ assets/src/fighter.js
@@ -64,6 +64,7 @@ class Fighter extends Sprite {
         }
     }
 
+    // Set attack state to true and move into attack animation
     attack() {
         this.isAttacking = true;
         this.switchSprite('attack1');
